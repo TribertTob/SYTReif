086fff6dca6c30abf5b441abad8a8af53a11c695
Tobias <tobias.tribert@gmail.com>
2019-11-29 08:14:42 +0100

    report

---
+++ GetStarted/report.h
@@ -0,0 +1,14 @@
+//
+// Created by Tobias on 22.11.2019.
+//
+
+#ifndef GETSTARTED_REPORT_H
+#define GETSTARTED_REPORT_H
+
+#include <stdbool.h>
+
+bool reportOpenFile(char *source);
+bool reportReport();
+bool reportCloseFile();
+
+#endif //GETSTARTED_REPORT_H
